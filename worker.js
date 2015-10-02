e1170a804019dc057c70ea37d455a26a9265dbc0
Cosmin Chiru <cosmin.chiru@gmail.com>
2015-10-02 11:17:45 +0300

    Moved the 2MB array generation into the worker.

--- worker.js
+++ worker.js
@@ -1,16 +1,38 @@
 // Import cryptographic libraries
 importScripts('libs/jsSHA/src/sha.js');
 
-onmessage = function(e) {
-	// Create a new jsSHA object
-	var shaObj = new jsSHA('SHA-256', 'TEXT');
-	
-	// Set the data to encrypt
-	shaObj.update(e.data);
-	
-	// Get hash
-	var hash = shaObj.getHash('HEX');
+onmessage = function(e) {	
+	if (e.data == "start") {		
+		// Our big array
+		var a = [];		
+		
+		// Create a 2MB array
+		for (var i = 2 * 1024 * 1024; i >= 0; i--) {
+		    a.push(i);
+		};
 
-	// Return hash
-	postMessage(hash);
+		// Get start time
+		start = new Date().getTime();
+
+		// Create a new jsSHA object
+		var shaObj = new jsSHA('SHA-256', 'TEXT');			
+
+		// Set the data to encrypt
+		shaObj.update(JSON.stringify(a));		
+		
+		// Get hash
+		var hash = shaObj.getHash('HEX');
+
+		// Get end time
+		var end = new Date().getTime();
+
+		// Get duration
+		var time = end - start;
+
+		// Create response
+		var response = '{"hash":"' + hash + '", "time":"' + time + '"}';
+
+		// Return response
+		postMessage(response);
+	}
 }
