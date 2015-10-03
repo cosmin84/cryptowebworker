2b574df2cca2d7c2361aa1be583694860edcecdd
Cosmin Chiru <cosmin.chiru@gmail.com>
2015-10-03 21:59:28 +0300

    Added MD5 encryption and optimised code overall.

--- worker.js
+++ worker.js
@@ -1,38 +1,51 @@
 // Import cryptographic libraries
 importScripts('libs/jsSHA/src/sha.js');
+importScripts('libs/SparkMD5/spark-md5.js');
 
-onmessage = function(e) {	
-	if (e.data == "start") {		
+self.onmessage = function(e) {	
+	if (e.data.command == 'start') {		
 		// Our big array
 		var a = [];		
 		
+		// A string with possible chars that will be used to generate a random char
+		var chars = '0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ';
+    	 
 		// Create a 2MB array
 		for (var i = 2 * 1024 * 1024; i >= 0; i--) {
-		    a.push(i);
+		    a.push(chars.substr( Math.floor(Math.random() * 62), 1));
 		};
 
-		// Get start time
-		start = new Date().getTime();
+		var s = JSON.stringify(a);
 
-		// Create a new jsSHA object
-		var shaObj = new jsSHA('SHA-256', 'TEXT');			
+		// Encrypt string
+		for (n = 0; n < e.data.iterations; n++) {
+			// Get start time
+			start = new Date().getTime();
 
-		// Set the data to encrypt
-		shaObj.update(JSON.stringify(a));		
-		
-		// Get hash
-		var hash = shaObj.getHash('HEX');
+			// Create a new jsSHA object
+			var shaObj = new jsSHA('SHA-1', 'TEXT');			
 
-		// Get end time
-		var end = new Date().getTime();
+			// Set the data to encrypt
+			shaObj.update(s);		
+			
+			// Get SHA-1 hash
+			var sha1_hash = shaObj.getHash('HEX');
 
-		// Get duration
-		var time = end - start;
+			// Get MD5 hash
+			var md5_hash = SparkMD5.hash(s);
 
-		// Create response
-		var response = '{"hash":"' + hash + '", "time":"' + time + '"}';
+			// Get end time
+			var end = new Date().getTime();
 
-		// Return response
-		postMessage(response);
+			// Get duration
+			var time = end - start;
+
+			// Create response
+			var response = '{"workerIndex":' + e.data.workerIndex + ', "iteration:' + n + ', time":"' + time + '"}';
+			
+			// Return response
+			self.postMessage(response);
+			
+		}
 	}
 }
